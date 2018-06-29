a6222656a486495286b6457d2314d8cbfc2e46e5
xquire24 <xquire24@gmail.com>
2018-06-29 15:44:22 +0000

    Write makeGrid function

--- designs.js
+++ designs.js
@@ -4,14 +4,27 @@ let color = $('#colorPicker').val();
 // Select size input and value
 let height = $('#inputHeight').val();
 let width = $('#inputWidth').val();
+
 // select canvas
 const canvas = document.getElementById('colorPicker').innerHTML;
 
-
 // When size is submitted by the user, call makeGrid()
 
+// function that creates grid and adds the selected color
 function makeGrid() {
+    //function that fills in color in a clicked cell
+    let addColor = function(cell){
+        cell.addEventListner('click', function(){
+            cell.style.backgroundColor = color;
+        });
+    }
 
-// Your code goes here!
-
+    //create grid and adds color to cell
+    for(let i=0; i<height; i++){
+        let row = canvas.insertRow(i);
+        for(let j=0; j<width; j++){
+            let cell = row.insertCell(j);
+            cell.addEventListner('click', addColor(cell));
+        }
+    }
 }
