071530f43863f8c054839dcc4d4e86beb514b4fd
xquire24 <xquire24@gmail.com>
2018-06-29 14:23:28 +0000

    Add variables

--- designs.js
+++ designs.js
@@ -1,5 +1,12 @@
-// Select color input
-// Select size input
+// Select color input and value
+let color = $('#colorPicker').val();
+
+// Select size input and value
+let height = $('#inputHeight').val();
+let width = $('#inputWidth').val();
+// select canvas
+const canvas = document.getElementById('colorPicker').innerHTML;
+
 
 // When size is submitted by the user, call makeGrid()
 
